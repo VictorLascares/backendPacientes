e4d1c101e1cc2ba600853d7119b674ec87a88ebf
VictorLascares <vmlascares@gmail.com>
2022-11-17 15:39:31 -0600

    Generando jsonwebtoken

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -1,4 +1,5 @@
 import Veterinario from "../models/Veterinario.js";
+import generarJWT from "../helpers/generarJWT.js";
 
 const registrar = async (req, res) => {
   const { email } = req.body;
@@ -64,7 +65,8 @@ const autenticar = async (req, res) => {
 
   // Revisar el password
   if (await usuario.comprobarPassword(password)) {
-    console.log("contraseña correcta");
+    // Autenticar
+    res.json({ token: generarJWT(usuario.id) });
   } else {
     const error = new Error("Contraseña incorrecta");
     return res.status(403).json({ msg: error.message });
