30abf784034484736d322748d0153b3e07224bab
VictorLascares <vmlascares@gmail.com>
2022-11-18 20:35:31 -0600

    Enviando la peticion para crear pacientes

--- controllers/pacienteController.js
+++ controllers/pacienteController.js
@@ -1,4 +1,9 @@
-const agregarPaciente = (req, res) => {};
+import Paciente from "../models/Paciente.js";
+
+const agregarPaciente = (req, res) => {
+  const paciente = new Paciente(req.body);
+  console.log(paciente);
+};
 
 const obtenerPacientes = (req, res) => {};
 
