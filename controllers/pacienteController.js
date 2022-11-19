0787822c71945a300c9d2cbc01d719466cff137c
VictorLascares <vmlascares@gmail.com>
2022-11-18 21:00:43 -0600

    Obteniendo los pacientes del veterinario

--- controllers/pacienteController.js
+++ controllers/pacienteController.js
@@ -2,7 +2,7 @@ import Paciente from "../models/Paciente.js";
 
 const agregarPaciente = async (req, res) => {
   const paciente = new Paciente(req.body);
-  paciente.veterinario = req.veterinario._id
+  paciente.veterinario = req.veterinario._id;
 
   try {
     const pacienteAlmacenado = await paciente.save();
@@ -12,6 +12,12 @@ const agregarPaciente = async (req, res) => {
   }
 };
 
-const obtenerPacientes = (req, res) => {};
+const obtenerPacientes = async (req, res) => {
+  const pacientes = await Paciente.find()
+    .where("veterinario")
+    .equals(req.veterinario);
+
+  res.json(pacientes);
+};
 
 export { agregarPaciente, obtenerPacientes };
